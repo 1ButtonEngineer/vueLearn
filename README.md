10d47445b53ab692688cb55d3b7ef22b41ccdf45
sy <1607276202@qq.com>
2019-09-26 18:12:30 +0800

    complete homeContainer

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 <<<<<<< HEAD
-# webProject
-=======
-#这是一个练习项目
+## 制作首页APP
+1.制作header区域，使用Mint-UI中的header组件
+2.制作底部Tabbar区域，使用的是MUI的Tabbar.html
+    +在制作 购物车 小图标的时候，操作会相对多一些；
+    +先把扩展图标的css样式，拷贝到项目中
+    +拷贝扩展字体库ttf文件，到项目中
+    +为购物车小图标添加如下样式 'mui-icon mui-icon-extra mui-icon-extra-cart'
+    +要在中间区域放置一个route-view来展示路有组件
 
-##打发尬舞无无无无无无无无无
->>>>>>> init my project
+##改造tabbar为router-link
+
+##设置路由高亮 在路由中设置  'linkActiveClass'
+
+##点击tabbar中路有的连接,展示对应的路有组件
+
+##制作首页轮播图布局
+
+##加载首页轮播图
+1.获取数据，使用vue-resoure获取数据。。。
+2.使用vue-resource 的 this.$http.get获取数据
+3.获取到的数据，要保存到data身上
+4.使用v-for循环渲染每个item选项
+
+##改造九宫格区域的样式
--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -4,18 +4,72 @@
         <!-- 轮播图区域 -->
 
         <mt-swipe :auto="4000">
+            <!-- 在组件中使用v-bind，必须使用key -->
             <mt-swipe-item>1</mt-swipe-item>
             <mt-swipe-item>2</mt-swipe-item>
             <mt-swipe-item>3</mt-swipe-item>
+
+            <!-- 因为缺少轮播图的api，这里不能使用 -->
+            <!-- <mt-swipe-item v-for="item in lunbotuList" :key="item.url">
+                <img src="item.img" alt="">
+            </mt-swipe-item> -->
+
         </mt-swipe>
 
 
-        <h3>HomeContainer</h3>
+        <!-- 图标 -->
+       
+        <ul class="mui-table-view mui-grid-view mui-grid-9">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <img src="../../images/menu1.png" alt="">
+                <div class="mui-media-body">新闻资讯</div></a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <img src="../../images/menu2.png" alt="">
+                <div class="mui-media-body">图片分享</div></a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+               <img src="../../images/menu3.png" alt="">
+                <div class="mui-media-body">商品购买</div></a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <img src="../../images/menu4.png" alt="">
+                <div class="mui-media-body">留言反馈</div></a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <img src="../../images/menu5.png" alt="">
+                <div class="mui-media-body">视频专区</div></a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+               <img src="../../images/menu6.png" alt="">
+                <div class="mui-media-body">联系我们</div></a></li>
+        </ul> 
     </div>
 </template>
 
 <script>
 
+import {Toast} from 'mint-ui'
+
+    export default {
+        data() {
+            return {
+                lunbotuList: []//保存轮播图的数组
+            }
+        },
+        created() {
+            // this.getLunbotu();
+        },
+        methods: {
+            getLunbotu() {
+                // this.$http.get("http://vue.studyit.io/api/getlunbo").then(result => {
+                //     //console.log(result.body);
+                //     if(result.body.status === 0) {
+                //         this.lunbotuList = result.body.message;
+
+                //     }else {
+                //         //获取图片失败
+                //         Toast('加载轮播图失败');
+                //     }
+                // })
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
@@ -34,6 +88,20 @@
         }
     }
     
+}
+
+.mui-grid-view.mui-grid-9 {
+    background-color: #fff;
+    border: none;
+    img {
+        width: 45px;
+    }
+    .mui-media-body {
+        font-size: 12px;
+    }
+}
+.mui-grid-view.mui-grid-9 .mui-table-view-cell {
+    border: none;
 }
 
 </style>
